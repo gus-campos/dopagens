2761cc62921725b9f0173df6417a1b92f54b9220
Gustavo Campos <gus.campos042@gmail.com>
2024-09-01 17:09:17 -0300

    docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-# sphinx-build -M html docs/source/ docs/build/
+# sphinx-build -b html docs/source/ docs/build/
 
 # Configuration file for the Sphinx documentation builder.
 #
